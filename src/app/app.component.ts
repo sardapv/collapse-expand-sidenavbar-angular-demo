294ebb7372a58bb2c2d3c9ff2da69f396b4d081b
sardapv <sardapranav1@gmail.com>
2021-08-10 23:18:58 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slidein', [
+      transition(':enter', [
+        // when ngif has true
+        style({ transform: 'translateX(-100%)' }),
+        animate(250, style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        // when ngIf has false
+        animate(250, style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
+})
+export class AppComponent {
+  expanded: boolean = true;
+}
